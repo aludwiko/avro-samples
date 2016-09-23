5bfa96685a81d5c43f935e688d0d9bc9d4ffb878
Giampaolo Trapasso <giampaolo.trapasso@radicalbit.io>
2016-09-23 11:53:56 +0200

    changes

--- src/main/scala/com/giampaolotrapasso/avrosamples/envelope/Envelope.scala
+++ src/main/scala/com/giampaolotrapasso/avrosamples/envelope/Envelope.scala
@@ -1,15 +1,6 @@
 package com.giampaolotrapasso.avrosamples.envelope
 
-import java.io.ByteArrayOutputStream
-
-import com.giampaolotrapasso.avrosamples.envelope.Envelopes.OrderWithEnvelope
-import com.giampaolotrapasso.avrosamples.events.MovieChangedV1
-import com.sksamuel.avro4s._
-import org.apache.avro.Schema
-import org.apache.avro.Schema.Field
-import org.apache.avro.generic.GenericRecord
-import org.apache.avro.io.{BinaryEncoder, EncoderFactory}
-import org.apache.avro.specific.SpecificDatumWriter
+import com.giampaolotrapasso.avrosamples.serializers.EventSerializer
 
 
 trait Message
@@ -22,59 +13,10 @@ case class Envelope[T <: Message](id: String,
                                   envType: String,
                                   version: Int,
                                   occurredOn: Long = System.currentTimeMillis(),
-                                  message: T)
+                                  message: T) extends EventSerializer[T]
 
 object Envelopes {
   type OrderWithEnvelope = Envelope[Order]
 }
 
 
-object EnvelopeSerializer {
-
-
-
-
-
-  def serializeOrder(o: Envelope[Order]): Array[Byte] = {
-
-    val format = RecordFormat[Envelope[Order]]
-
-    val writer = new SpecificDatumWriter[GenericRecord](schema)
-    val out = new ByteArrayOutputStream()
-    val encoder: BinaryEncoder = EncoderFactory.get().binaryEncoder(out, null)
-    writer.write(genericUser, encoder)
-    encoder.flush()
-    out.close()
-
-    val serializedBytes: Array[Byte] = out.toByteArray()
-
-
-
-      val output = new ByteArrayOutputStream
-
-
-    // record is of type GenericRecord
-    val record: GenericRecord = format.to(o)
-
-
-
-      val avro   = AvroOutputStream.data[GenericRecord](output)
-      avro.write(o.asInstanceOf[GenericRecord])
-      avro.close()
-      output.toByteArray
-    }
-/*
-  def serializePayments(o: AnyRef): Array[Byte] = {
-    implicit val schemaFor = SchemaFor[Envelope[Payment]]
-
-    val output = new ByteArrayOutputStream
-    val avro   = AvroOutputStream.data[Envelope[Payment]](output)
-    avro.write(o.asInstanceOf[Envelope[Payment]])
-    avro.close()
-    output.toByteArray
-  }
-  */
-
-
-
-}
---
+++ src/main/scala/com/giampaolotrapasso/avrosamples/serializers/EventSerializer.scala
@@ -0,0 +1,42 @@
+package com.giampaolotrapasso.avrosamples.serializers
+
+import java.io.ByteArrayOutputStream
+
+import com.sksamuel.avro4s._
+import org.apache.avro.file.SeekableByteArrayInput
+import org.apache.avro.generic.{GenericDatumReader, GenericDatumWriter, GenericRecord}
+import org.apache.avro.io.{DecoderFactory, EncoderFactory}
+
+abstract class EventSerializer[T: ToRecord: FromRecord: SchemaFor] {
+
+  type Event = T
+  val eventSchema = implicitly[SchemaFor[T]].apply()
+
+  protected val format = RecordFormat[T]
+
+  protected val eventDatumWriter = new GenericDatumWriter[GenericRecord](eventSchema)
+  protected val eventDatumReader = new GenericDatumReader[GenericRecord](eventSchema, eventSchema)
+
+  //override val identifier: Int = EventSerializer.generateIdentifier()
+
+  //override def manifest(o: AnyRef): String = o.getClass.getSimpleName
+
+   def toBinary(o: AnyRef): Array[Byte] = {
+
+    AvroOutputStream
+    val output = new ByteArrayOutputStream()
+    val encoder = EncoderFactory.get().binaryEncoder(output, null)
+    val record = format.to(o.asInstanceOf[T])
+    eventDatumWriter.write(record, encoder)
+    encoder.flush()
+    output.close()
+    output.toByteArray
+  }
+
+   def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
+    val input = new SeekableByteArrayInput(bytes)
+    val decoder = DecoderFactory.get().binaryDecoder(input, null)
+    val record = eventDatumReader.read(null, decoder)
+    format.from(record).asInstanceOf[AnyRef]
+  }
+}
